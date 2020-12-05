c9206f206bfdb10cdce4d9f21b74f09457c24bf9
stepJo <ephenjo99@gmail.com>
2020-12-05 13:05:24 +0700

    Refactoring and add remember me

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Auth;
+use Cookie;
+use App\User;
 
 class LoginController extends Controller
 {
@@ -36,5 +40,29 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $credential = [
+            'email'    => $request->email,
+            'password' => $request->password,
+        ];
+
+        if(Auth::attempt($credential))
+        {
+            $user = User::where(["email" => $credential['email']])->first();
+            
+            Auth::login($user);
+
+            Cookie::queue('email', $user->email, 120);
+            Cookie::queue('password', $request->password, 120);
+
+            return redirect()->route('home');
+        }
+        else
+        {
+            return redirect()->route('login')->with('failed', 'User not found');
+        }
     }
 }
